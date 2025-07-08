57fb5c2700d2018c0504e9d5e34b79f1f6c60745
arshad-v <arshadv4422@gmail.com>
2025-07-08 16:29:00 +0530

    pdf uploading embedding

---
+++ vector_db.py
@@ -0,0 +1,17 @@
+# vector_db.py
+
+import streamlit as st
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+
+class VectorStore:
+    def __init__(self, text_chunks):
+        self.text_chunks = text_chunks
+        self.embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+    def build_faiss_index(self):
+        """Create and return a FAISS index from text chunks."""
+        with st.spinner("🔍 Creating semantic vector index..."):
+            return FAISS.from_documents(self.text_chunks, self.embeddings)
